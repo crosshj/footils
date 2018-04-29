2672a72fb8c2f4d1487a7b0b6e1613b52dad66b1
HJ Cross <github@chimpjuice.com>
2018-04-28 23:16:20 -0400

    rxReact and examples

--- web-foo.js
+++ web-foo.js
@@ -20,15 +20,54 @@
             - refer to docs for context / use case
     */
 
-    const isInitedFactory = function(parent, callback){
-        if(parent.isInited){
-            console.log(`${parent.name}: script already inited!`);
-        }
-        parent.isInited = true;
-        //TODO:  append script(s), etc
+   function loadScript(scriptUrl){
+        return new Promise(function (res, rej) {
+            let script = document.createElement('script');
+            script.src = scriptUrl;
+            script.type = 'text/javascript';
+            script.onError = rej;
+            script.async = true;
+            script.onload = res;
+            script.addEventListener('error',rej);
+            script.addEventListener('load',res);
+            document.head.appendChild(script);
+        });
+    }
+
+    function consoleOrCallback(callback){
         if(callback && typeof callback === 'function'){
-            callback();
+            return callback;
         }
+        return (error, data) => {
+            if (error){
+                console.error(error);
+                return;
+            }
+            if (data){
+                console.log(data);
+            }
+        }
+    }
+
+    const isInitedFactory = function(parent, callback){
+        const cb = consoleOrCallback(callback);
+        if(parent.isInited){
+            cb(`${parent.name}: script already inited!`);
+        }
+        var scripts = parent.scripts || [];
+        Promise.all(scripts.map(s => loadScript(s)))
+            .then(() => {
+                parent.isInited = true;
+                if(parent.scriptsAfter){
+                    parent.scriptsAfter(cb);
+                    return;
+                }
+                cb(null, `${parent.name}: script is inited!`);
+            })
+            .catch(() => {
+                const errorMessage = `error initialising ${parent.name}`;
+                cb(errorMessage);
+            });
     }
 
     const returnProps = function(o){
@@ -38,21 +77,77 @@
         }, {})
     }
 
+    // GITHUBPAGES -------------------------------------------------------------
     function githubPages(){ return returnProps(githubPages); }
     githubPages.scripts = [
         'https://crosshj.com/experiments/ghPageHelper.js'
     ];
     githubPages.init = callback => isInitedFactory(githubPages, callback);
 
+
+
+    // RXREACT -----------------------------------------------------------------
     function rxReact(){ return returnProps(rxReact); }
     rxReact.scripts = [
         'https://unpkg.com/rxjs@beta/bundles/rxjs.umd.js',
         'https://unpkg.com/react@16/umd/react.development.js',
         'https://unpkg.com/react-dom@16/umd/react-dom.development.js'
     ];
-    // TODO: include helpers that set everything up except the basic needs: REDUCER / VIEW / ROOT
+    rxReact.scriptsAfter = (callback) => {
+        const { of, fromEvent, from, range } = rxjs;
+        const { Observable, Subject } = rxjs;
+        const { map, filter, startWith, scan } = rxjs.operators;
+        const { render } = ReactDOM;
+        const { createElement, Component } = React;
+
+        const components = {
+            div: (props, children) => createElement("div", props, children),
+            textarea: (props, children) => createElement("textarea", props, children),
+            h4: (props, children) => createElement("h4", props, children),
+            h1: (props, children) => createElement("h1", props, children),
+            fragment: children => createElement(Fragment, null, children)
+        };
+
+        const action$ = new Subject();
+        const dispatcher = function(action){
+          return action$.next(action);
+        }
+
+        function RXConnector(props) {
+            this.state = {};
+            this.render = () => {
+                return fragment(props.render(this.state));
+            };
+            this.observable$ = props.observable$;
+            this.componentWillMount = function(){
+                this.observable$.subscribe(o => {
+                this.setState(o)
+                });
+            }
+        }
+        RXConnector.prototype = Object.create(Component.prototype);
+        const Connector = (props, children) => createElement(RXConnector, props, children);
+
+        const start = ({reducer, root, attach}) => {
+            const initialState = {};
+            const store$ = action$
+              .pipe(
+                scan(reducer, initialState)
+              );
+
+            render(
+                Connector({ observable$: store$, render: root }),
+                attach
+            );
+        };
+
+        callback(null, { components, dispatcher, start, React, rxjs });
+    };
     rxReact.init = callback => isInitedFactory(githubPages, callback);
 
+
+
+    // CANVAS ------------------------------------------------------------------
     function canvas(){ return returnProps(canvas); }
     canvas.scripts = [
         'https://crosshj.com/sandbox/canvas_plus.js'
