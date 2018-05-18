2c5f289b44679d1a5a7154af0a62bc3ede81239a
HJ Cross <github@chimpjuice.com>
2018-05-18 00:07:43 -0400

    eye toggle for layers

--- web-foo.js
+++ web-foo.js
@@ -373,6 +373,25 @@
                     );
                 }
 
+                function eyeToggle(){
+                    return (
+                        svg({
+                            xmlns:"http://www.w3.org/2000/svg",
+                            'xmlns:xlink': "http://www.w3.org/1999/xlink",
+                            version: "1.1",
+                            viewBox: "0 0 512 512",
+                            'enable-background': "new 0 0 512 512"
+                        },
+                            g(null, [
+                                path({
+                                    d: "m34,256l26.2,26.2c108,108 283.7,108 391.7,0l26.1-26.2-26.2-26.2c-108-108-283.7-108-391.7,0l-26.1,26.2zm222,126.2c-75.8,0-151.6-28.9-209.3-86.6l-32.9-32.9c-3.7-3.7-3.7-9.7 0-13.5l32.9-32.9c115.4-115.4 303.2-115.4 418.6,0l32.9,32.9c3.7,3.7 3.7,9.7 0,13.5l-32.9,32.9c-57.7,57.7-133.5,86.6-209.3,86.6z"
+                                }),
+                                circle({ cx:"256", cy: "256", r: "80"})
+                            ])
+                        )
+                    );
+                }
+
                 function layersComponent({ div, span, section, item, index}){
                     return (
                         div({
