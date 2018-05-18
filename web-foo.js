95d4e6ef5d301bda87b99deceb6fadbbdb82a855
HJ Cross <github@chimpjuice.com>
2018-05-18 00:37:48 -0400

    not visible line for eye toggle

--- web-foo.js
+++ web-foo.js
@@ -373,7 +373,7 @@
                     );
                 }
 
-                function eyeToggle({ svg, g, path, circle }){
+                function eyeToggle({ svg, g, path, circle, visible=true }){
                     return (
                         svg({
                             xmlns:"http://www.w3.org/2000/svg",
@@ -381,13 +381,19 @@
                             version: "1.1",
                             viewBox: "0 0 512 512",
                             preserveAspectRatio: "none",
-                            className: "eyeToggle"
+                            className: "eyeToggle",
+                            tabIndex: 0
                         },
                             g(null, [
                                 path({
                                     d: "m34,256l26.2,26.2c108,108 283.7,108 391.7,0l26.1-26.2-26.2-26.2c-108-108-283.7-108-391.7,0l-26.1,26.2zm222,126.2c-75.8,0-151.6-28.9-209.3-86.6l-32.9-32.9c-3.7-3.7-3.7-9.7 0-13.5l32.9-32.9c115.4-115.4 303.2-115.4 418.6,0l32.9,32.9c3.7,3.7 3.7,9.7 0,13.5l-32.9,32.9c-57.7,57.7-133.5,86.6-209.3,86.6z"
                                 }),
-                                circle({ cx:"256", cy: "256", r: "80"})
+                                circle({ cx:"256", cy: "256", r: "80"}),
+                                !visible
+                                    ? path({
+                                        d: "M 400 52 L 460 52 L 160 460 L 100 460 Z"
+                                    })
+                                    : null
                             ])
                         )
                     );
@@ -436,15 +442,15 @@
                                     tabIndex: 0
                                 }, [
                                     eyeToggle({ svg, g, path, circle }),
-                                    div({ className: "image"}),
-                                    span({ className: "label"}, 'Layer 1')
+                                    div({ className: "image", tabIndex: 0}),
+                                    span({ className: "label", tabIndex: 0}, 'Layer 1')
                                 ]),
                                 li({
                                     tabIndex: 0
                                 }, [
                                     eyeToggle({ svg, g, path, circle }),
-                                    div({ className: "image"}),
-                                    span({ className: "label"}, 'Layer 2')
+                                    div({ className: "image", tabIndex: 0}),
+                                    span({ className: "label", tabIndex: 0}, 'Layer 2')
                                 ])
                             ])
                         ])
