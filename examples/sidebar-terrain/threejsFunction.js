140f413b1257b6b1d5677784cabf59050035e765
HJ Cross <github@chimpjuice.com>
2018-09-14 23:18:06 -0400

    cleanup terrain example

--- examples/sidebar-terrain/threejsFunction.js
+++ examples/sidebar-terrain/threejsFunction.js
@@ -1,5 +1,4 @@
 (function(){
-    // https://resources.oreilly.com/examples/9781784392215/blob/dd019759a7b48fe409b13f232abb1b0ce988cf5e/learning-threejs-master/chapter-04/04-mesh-normal-material.html
 
     const glFunction = (() => {
         console.log({ ctx, gl, alpha, width, height});
@@ -44,33 +43,12 @@
         var ambientLight = new THREE.AmbientLight(0xffffff);
         scene.add(ambientLight);
 
-        // add spotlight for the shadows
-        // var spotLight = new THREE.SpotLight(0xffffff);
-        // spotLight.position.set(0, 50, 10);
-        // spotLight.castShadow = true;
-        // spotLight.shadow.mapSize.width = 1024;
-        // spotLight.shadow.mapSize.height = 1024;
-        // scene.add(spotLight);
-
-        var helper = new THREE.CameraHelper( light.shadow.camera );
-        //scene.add( helper );
-
-        //var groundMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff } );
-
-        //var mat_wireframe = new THREE.MeshBasicMaterial({color: 0x000000, wireframe: true});
-        //var mat_lambert = new THREE.MeshLambertMaterial({color: 0xffffff, shading: THREE.FlatShading});
-        //var cubeMaterial = [ mat_wireframe, mat_lambert ];
 
         var t = new THREE.Texture(document.getElementById('pattern-needles'));
         t.repeat.set(1, 1);
         t.needsUpdate = true;
         t.anisotropy = 32;
-        //t.rotation = (90 * Math.PI)/180;
-        // var cubeMaterial = new THREE.MeshBasicMaterial({
-        //     map: t
-        // });
 
-        // tree
         var cubeMaterial = new THREE.MeshPhongMaterial( {
             color: 0x8899dd, specular: 0x8bc34a, shininess: 1,
             map: t
@@ -80,21 +58,10 @@
         var myTexture = new THREE.Texture(document.getElementById('pattern-stones'));
         myTexture.needsUpdate = true;
 
-        // var shader = THREE.ShaderLib[ "normalmap" ];
-        // var uniforms = THREE.UniformsUtils.clone( shader.uniforms );
-        // uniforms[ "enableDisplacement" ].value = true;
-        // uniforms[ "enableDiffuse" ].value = true;
-        // uniforms[ "tDisplacement" ].value = myTexture;
-        // uniforms[ "tDiffuse" ].value = myTexture;
-        // uniforms[ "uDisplacementScale" ].value = 50;
-
-        // var parameters = { fragmentShader: shader.fragmentShader, vertexShader: shader.vertexShader, uniforms: uniforms, lights: true, wireframe: true };
-        //var groundMaterial = new THREE.ShaderMaterial( parameters );
-
         var dMap = new THREE.Texture(document.getElementById('pattern-displace'));
-				dMap.repeat.set(1, 1);
-				dMap.needsUpdate = true;
-				dMap.anisotropy = 32;
+        dMap.repeat.set(1, 1);
+        dMap.needsUpdate = true;
+        dMap.anisotropy = 32;
 
         var groundMaterial = new THREE.MeshStandardMaterial( {
             //color: 0x333333,
@@ -118,50 +85,11 @@
         scene.add(groundMesh);
 
 
-
-
-
         // position and point the camera to the center of the scene
         camera.position.x = 0;
         camera.position.y = 50;
         camera.position.z = 160;
         camera.lookAt(new THREE.Vector3(0, 35, 5));
-
-        var camhelper = new THREE.CameraHelper( camera );
-        //scene.add( camhelper );
-
-        var axesHelper = new THREE.AxesHelper( 5 );
-        //scene.add( axesHelper );
-
-        // var loader = new THREE.OBJLoader();
-        // const tree = loader.parse(window.treeModel);
-
-        // tree.traverse( function ( child ) {
-        //     if ( child instanceof THREE.Mesh ) {
-        //         child.material = child.material.map(material => {
-        //             if(material.name === 'Bark'){
-        //                 return sphereMaterial;
-        //             }
-        //             if(material.name === 'Tree'){
-        //                 return cubeMaterial;
-        //             }
-        //             return material;
-        //         });
-        //         child.material.overdraw = 1
-
-        //         //var geometry = new THREE.Geometry().fromBufferGeometry( child.geometry );
-        //         //geometry.computeFaceNormals();
-        //         //geometry.mergeVertices();
-        //         //geometry.computeVertexNormals();
-        //         //modifier.modify(geometry);
-        //         //child.geometry = new THREE.BufferGeometry().fromGeometry( geometry );
-
-        //         child.castShadow = true;
-        //         child.receiveShadow = true;
-        //     }
-        // } );
-
-
 
         renderer.render(scene, camera);
 
